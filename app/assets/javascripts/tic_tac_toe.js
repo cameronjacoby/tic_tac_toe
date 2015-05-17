5a4662c7f598d78f5ed087244166398bec65982f
cameronjacoby <cameron.jacoby@gmail.com>
2015-05-17 15:09:10 -0700

    implement basic game board front-end

--- app/assets/javascripts/tic_tac_toe.js
+++ app/assets/javascripts/tic_tac_toe.js
@@ -5,4 +5,41 @@ $(function() {
     history.pushState('', document.title, window.location.pathname);
   }
 
+  var avatars = {
+    0: "/assets/cupcake.jpg",
+    1: "/assets/doge.jpg"
+  };
+
+  var player = parseInt($('#player_index').val());
+  var playerTurn = false;
+
+  // first player (index 0) gets to start
+  if (player === 0) {
+    playerTurn = true;
+  }
+
+  $('.game_col').on('mouseover', function() {
+    if (playerTurn && !$(this).hasClass('filled')) {
+      $(this).children('.avatar').attr('src', avatars[player]);
+      $(this).children('.avatar').fadeIn('fast');
+    }
+  });
+
+  $('.game_col').on('mouseleave', function() {
+    if (!$(this).hasClass('filled')) {
+      $(this).children('.avatar').hide();
+      $(this).children('.avatar').attr('src', "");
+    }
+  });
+
+  $('.game_col').on('click', function() {
+    if (playerTurn && !$(this).hasClass('filled')) {
+      $(this).children('.avatar').attr('src', avatars[player]);
+      $(this).children('.avatar').removeClass('add_opacity');
+      $(this).children('.avatar').fadeIn('fast');
+      $(this).addClass('filled');
+      playerTurn = false;
+    }
+  });
+
 });
--- config/application.rb
+++ config/application.rb
@@ -21,6 +21,7 @@ module TicTacToe
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
+    config.assets.enabled = true
     config.assets.paths << Rails.root.join('vendor', 'assets', 'components')
 
   end
